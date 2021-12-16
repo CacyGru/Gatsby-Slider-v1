fe01862c3a7b7f894bf7101ed6367b649d064046
cacygru <c.grueter@gmx.de>
2021-12-16 12:12:37 +0100

    Gatsby Slider v1 with arrows

---
+++ src/components/nav.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function nav() {
+    return (
+        <div className='header_content'>
+            
+            <logo>The Logo</logo>
+            <div className="nav">
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                </ul>
+            </div>
+            
+
+        </div>
+    )
+}
+
+export default nav
